f117cfc630b1aa7d5ae80e7740a15b107d331fd6
Randaaayman <randaaysamir04@gmail.com>
2025-03-29 23:40:32 +0200

    LED.C

--- LED.C
+++ LED.C
@@ -5,13 +5,20 @@ void RGB_Output(uint32_t data) {
     GPIO_PORTF_DATA_R = data;
 }
 
-void LED_SetColor( distance) {  // Explicitly define distance type
-    if (distance >= 5) {
-        RGB_Output(0x02); // Red LED
-    } else if (distance > 0) {
-        RGB_Output(0x04); // Yellow LED
-    } else {
-        RGB_Output(0x08); // Green LED
+void LED_SetColor(distance) {
+    switch (1) {  
+        case (distance > 5):
+            RGB_Output(0x02); // Red LED
+            break;
+        case (distance > 2.5):
+            RGB_Output(0x04); // Yellow LED
+            break;
+        case (distance > 0):
+            RGB_Output(0x08); // Green LED
+            break;
+        default:
+            RGB_Output(0x00); // LEDs Off
+            break;
     }
 }
 
