fc323b349ef40ceffa42ae317c0f682102e12df0
Randaaayman <randaaysamir04@gmail.com>
2025-03-29 22:35:00 +0200

    LED.C

---
+++ LED.C
@@ -0,0 +1,21 @@
+#include "led.h"
+
+void LED_SetColor(u8 distance) {
+    if (distance >= 5) {
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, RED_LED, GPIO_HIGH);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, YELLOW_LED, GPIO_LOW);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, GREEN_LED, GPIO_LOW);
+
+    } else if (distance > 0 && distance < 5) {
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, RED_LED, GPIO_LOW);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, YELLOW_LED, GPIO_HIGH);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, GREEN_LED, GPIO_LOW);
+
+    } else {
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, RED_LED, GPIO_LOW);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, YELLOW_LED, GPIO_LOW);
+        MCAL_GPIO_SetPinValue(GPIO_PORTF, GREEN_LED, GPIO_HIGH);
+    }
+}
+
+
