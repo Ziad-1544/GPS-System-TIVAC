615c6b8f20bddb4dac272bcfa541482b91736a0e
Randaaayman <randaaysamir04@gmail.com>
2025-04-01 20:24:05 +0200

    lcd.h

--- lcd.h
+++ lcd.h
@@ -12,17 +12,17 @@
 #define RS  0x01  
 #define EN  0x04  
 
-#define lcd_Clear            0x01          
-#define lcd_Home             0x02          
-#define lcd_EntryMode        0x06         
-#define lcd_DisplayOff       0x08          
-#define lcd_DisplayOn        0x0F          
-#define Set5x7FontSize     0x20					
-#define four_bit_mode		 0x28					     
-#define first_row            0x80         
-#define second_row           0xC0          
-#define CURSOR_OFF           0x0C					 
-#define CURSOR_ON            0x0E					 
+#define lcd_Clear              0x01          
+#define lcd_Home               0x02          
+#define lcd_EntryMode          0x06         
+#define lcd_DisplayOff         0x08          
+#define lcd_DisplayOn          0x0F          
+#define Set5x7FontSize         0x20					
+#define four_bit_mode		   0x28					     
+#define first_row              0x80         
+#define second_row             0xC0          
+#define CURSOR_OFF             0x0C					 
+#define CURSOR_ON              0x0E					 
 
 
 #include "GPIO_Interface.h"
