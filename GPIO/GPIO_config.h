414c192a5f16b4c5473fa238be5f144f4a59cb1d
ziad kassem <ziad.kassem.eng@gmail.com>
2025-04-10 00:42:44 +0200

    pushing gpio

---
+++ GPIO/GPIO_config.h
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////
+////////      Ain Shams University        /////////
+///////     Intro To Embedded Project    /////////
+//////           Layer:  MCAL           /////////
+/////                GPIO              /////////
+////                Version:1.0       /////////
+///         DATE:   4-7-2025         /////////
+//         AUTHOR: Ziad Kassem      /////////
+////////////////////////////////////////////
+#ifndef GPIO_CONFIG_H
+#define GPIO_CONFIG_H
+
+#endif
