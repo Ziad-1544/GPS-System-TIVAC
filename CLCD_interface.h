4e1e2c617c4b1acbd7fa89a1d8b80e9fae8afd8a
Randa Ayman <randaaysamir04@gmail.com>
2025-04-13 05:00:26 +0200

    CLCD_interface.h

--- CLCD_interface.h
+++ CLCD_interface.h
@@ -9,37 +9,21 @@
 #ifndef LCD_INTERFACE_h_
 #define LCD_INTERFACE_h_
 
-#include "lcd_config.h"
-#include "lcd_private.h"
+#include "CLCD_config.h"
+#include "CLCD_private.h"
 #include "GPIO_Interface.h"
-#include "GPIO_config.h"
-#include "GPIO_private.h"
-//.........................................................................................Macro declarations(character lcd interfacing)....................................................................................................//
- 
 
 
-#define ROW1 1
-#define ROW2 2
-#define ROW3 3
-#define ROW4 4
 
 
-typedef struct{
-    pin_config_t lcd_rs;
-    pin_config_t lcd_en;
-    pin_config_t lcd_data[8];
+STD_ERROR CLCD_8DataPinsInit (void);
 
-}chr_lcd_8bit_t;
 
-STD_ERROR lcd_8bit_intialize(const chr_lcd_8bit_t *lcd);
-STD_ERROR lcd_8bit_send_command(const chr_lcd_8bit_t *lcd, uint8 command);
-STD_ERROR lcd_8bit_send_char_data(const chr_lcd_8bit_t *lcd, uint8 data);
-STD_ERROR lcd_8bit_send_char_data_pos(const chr_lcd_8bit_t *lcd, uint8 row, uint8 column, uint8 data);
-STD_ERROR lcd_8bit_send_string(const chr_lcd_8bit_t *lcd, uint8 *str);
-STD_ERROR lcd_8bit_send_string_pos(const chr_lcd_8bit_t *lcd, uint8 row, uint8 column, uint8 *str);
-STD_ERROR convert_uint8_to_string(uint8 value, uint8 *str);
-STD_ERROR convert_uint16_to_string(uint16 value, uint8 *str);
-STD_ERROR convert_uint32_to_string(uint32 value, uint8 *str);
-
+STD_ERROR CLCD_8DATASendString( u8 *str);
+STD_ERROR CLCD_8DATASetCursor( u8 row, u8 column);
+STD_ERROR convert_uint16_to_string(u16 value);
+void CLCD_voidClearDisplay();
+void CLCD_voidSendSpecialCharacter(u8 Copy_u8BlockNum, u8 Copy_HorizontalValue,u8 copy_AbsVerticalValue,u8 *Pu8ArrayPattern);
+void CLCD_SendNum(u16 Num);
 
 #endif
