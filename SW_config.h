063d35deda7a693ca8a07639b9283e9eb1c8bd10
Randa Ayman <randaaysamir04@gmail.com>
2025-04-17 15:58:37 +0200

    uploading in seperate files

--- SW_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef SW_CONFIG_H
-#define SW_CONFIG_H
-
-#define SW_PORT      GPIO_PORTD_BASE   // Using Port D for switches
-#define SW1_BIT      0x10              // PD4 (0b00010000)
-#define SW2_BIT      0x01              // PD0 (0b00000001)
-
-#endif
--- SW_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef SW_INTERFACE_H
-#define SW_INTERFACE_H
-
-void SW_Init(void);           // Initializes all used switch pins (like PD4, PD0)
-u8 SW_input(void);            // Reads all switch values and returns combined result
-
-#endif
