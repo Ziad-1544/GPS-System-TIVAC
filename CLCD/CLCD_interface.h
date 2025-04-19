70b6317dc9407de0cfea6240b01ea7f5edae34fc
Randa Ayman <randaaysamir04@gmail.com>
2025-04-19 16:43:43 +0200

    CLCD_interface.h

--- CLCD/CLCD_interface.h
+++ CLCD/CLCD_interface.h
@@ -22,7 +22,7 @@ STD_ERROR CLCD_8DataPinsInit (void);
 STD_ERROR CLCD_8DATASendString( u8 *str);
 STD_ERROR CLCD_8DATASetCursor( u8 row, u8 column);
 STD_ERROR convert_uint16_to_string(u16 value);
-void CLCD_voidClearDisplay();
+void CLCD_ClearDisplay();
 void CLCD_voidSendSpecialCharacter(u8 Copy_u8BlockNum, u8 Copy_HorizontalValue,u8 copy_AbsVerticalValue,u8 *Pu8ArrayPattern);
 void CLCD_SendNum(u16 Num);
 
