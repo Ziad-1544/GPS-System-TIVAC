9030df89e7e91edfe0bbd69186f94cfc4433a8fa
Randa Ayman <randaaysamir04@gmail.com>
2025-04-19 16:53:35 +0200

    LED.h

--- LED/LED.h
+++ LED/LED.h
@@ -14,11 +14,14 @@
 // Define LED port
 #define LED_PORT   GPIO_PORTC 
 
-#define RED_LED    0x02
-#define YELLOW_LED 0x0A
-#define GREEN_LED  0x08
+#define RED_LED       0x01  // PC0
+#define GREEN_LED     0x02  // PC1
+#define YELLOW_LED    0x03  
+#define EXTERNAL_LED  0x04 
 
 void LED_SetColor(distance);    //don't forget  distance from gps
+
+void LED_ExternalControl(u32 Port, u8 PinId);
 
 #endif
 
