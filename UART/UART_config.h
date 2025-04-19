4b9417394e420ea69db0d0a3855a7156b906fa65
Ahmad Ezz ElDeen <ahmadezzeldeen3@gmail.com>
2025-04-19 16:11:32 +0200

    UART V1.0

---
+++ UART/UART_config.h
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////
+////////        Ain Shams University                    /////////
+///////       Intro To Embedded Project                /////////
+//////            Layer:  MCAL                        /////////
+/////                UART                            /////////
+////            Version:1.0                         /////////
+///          DATE:   4-19-2025                     /////////
+//        AUTHOR: Ahmad Ayman , Hussein Bahaa     /////////
+//////////////////////////////////////////////////////////
+
+#ifndef UART_CONFIG_H
+#define UART_CONFIG_H
+
+
+
+#endif
