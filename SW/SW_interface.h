ed21304f4e90b10550177af711c76db536b0edea
Randa Ayman <randaaysamir04@gmail.com>
2025-04-20 10:59:22 +0200

    SW_interface.h

--- SW/SW_interface.h
+++ SW/SW_interface.h
@@ -5,8 +5,20 @@
 #include "GPIO_Interface.h"
 #include "SW_private.h"
 #include "SW_config.h"
+#include "STD_TYPES.h"
 
-void SW_Init(void);           // Initializes all used switch pins (like PD4, PD0)
-u8 SW_input(void);            // Reads all switch values and returns combined result
+
+
+#define SW_PRESSED      1
+#define SW_NOT_PRESSED  0
+
+// Internal switch functions
+void SW_Init(u8 Port, u8 Pin, GPIO_ResType_t ResType);
+STD_ERROR SW_GetRawValue(u8 Port, u8 Pin, u8* Copy_PinValue);
+u8 SW_IsPressed(u8 Port, u8 Pin, GPIO_ResType_t ResType);
+
+// External switch functions
+void EXTSW_Init(u8 Port, u8 Pin);
+u8 EXTSW_IsPressed(u8 Port, u8 Pin, GPIO_ResType_t ResType);
 
 #endif
