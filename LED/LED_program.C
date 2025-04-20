c0b5446580a962c2cfe11c0e2849c855e24ffa1f
Randa Ayman <randaaysamir04@gmail.com>
2025-04-20 11:44:07 +0200

    LED_program.C

--- LED/LED_program.C
+++ LED/LED_program.C
@@ -6,49 +6,58 @@
 /////           CREATED:28/03/2025     ////////
 ////////////////////////////////////////////// 
 
-#include"GPIO_Interface.h"
 #include "LED_interface.h"
 #include "LED_private.h"
+#include "GPIO_Interface.h"
 
-
-static void LED_ClearAll(void)
+void LED_InitInternal(void)
 {
-    GPIO_WritePin(LED_PORT, RED_LED, 0);   // Turn off red
-    GPIO_WritePin(LED_PORT, GREEN_LED, 0); // Turn off green
+    GPIO_INIT(LED_PORT);
+    GPIO_SetPinDir(LED_PORT, RED_LED_PIN, PIN_OUTPUT);
+    GPIO_SetPinDir(LED_PORT, GREEN_LED_PIN, PIN_OUTPUT);
+
+    // Default all off
+    GPIO_WritePin(LED_PORT, RED_LED_PIN, LED_OFF);
+    GPIO_WritePin(LED_PORT, GREEN_LED_PIN, LED_OFF);
 }
 
-void LED_ExternalControl(u32 InputPort, u8 InputPin)
+void LED_SetInitialValue(u8 LedID, u8 LedState)
 {
-    u8 inputValue;
-    GPIO_ReadPin(InputPort, InputPin, &inputValue);
-
-    if (inputValue)
+    switch (LedID)
     {
-        GPIO_WritePin(LED_PORT, EXTERNAL_LED, 1);  // Turn ON external LED
-    }
-    else
-    {
-        GPIO_WritePin(LED_PORT, EXTERNAL_LED, 0);  // Turn OFF external LED
+    case RED_LED:
+        GPIO_WritePin(LED_PORT, RED_LED_PIN, LedState);
+        break;
+    case GREEN_LED:
+        GPIO_WritePin(LED_PORT, GREEN_LED_PIN, LedState);
+        break;
+    case YELLOW_LED:
+        GPIO_WritePin(LED_PORT, RED_LED_PIN, LedState);
+        GPIO_WritePin(LED_PORT, GREEN_LED_PIN, LedState);
+        break;
+    default:
+        // Do nothing for EXTERNAL_LED, handled manually via GPIO
+        break;
     }
 }
 
-//void LED_SetColor(distance) {
-  
-//    int category = (distance > 5) * 3 + (distance > 2.5 && distance <= 5) * 2 + (distance > 0 && distance <= 2.5) * 1;
-
-//    switch (category) {  
-   //     case 3:  
-    //        RGB_Output(0x02); // Red LED  
-    //        break;  
-    //    case 2:  
-    //        RGB_Output(0x0A); // Yellow LED  
-   //         break;  
-     //   case 1:  
-     //       RGB_Output(0x08); // Green LED  
-     //       break;  
-     //   default:  
-     //       RGB_Output(0x00); // LEDs Off  
-      //      break;  
-    }  
+void LED_InitExternal(u8 Port, u8 Pin)
+{
+    GPIO_INIT(Port);
+    GPIO_SetPinDir(Port, Pin, PIN_OUTPUT);
+    GPIO_WritePin(Port, Pin, LED_OFF);
 }
 
+STD_ERROR LED_GetStatus(u8 Port, u8 Pin, u8* Status)
+{
+    return GPIO_ReadPin(Port, Pin, Status);
+}
+
+void LED_Toggle(u8 Port, u8 Pin)
+{
+    u8 currentState = 0;
+    if (GPIO_ReadPin(Port, Pin, &currentState) == OK)
+    {
+        GPIO_WritePin(Port, Pin, !currentState);
+    }
+}
