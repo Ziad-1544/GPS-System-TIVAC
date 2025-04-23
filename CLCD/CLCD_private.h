304948eca2f57395c7b6e7f151b1d0c29dc2b9e9
Randa Ayman <randaaysamir04@gmail.com>
2025-04-23 23:11:11 +0200

    CLCD_private.h

--- CLCD/CLCD_private.h
+++ CLCD/CLCD_private.h
@@ -12,15 +12,15 @@
 #define CLCD_PRIVATE_h_
 
 
-#define lcd_Clear              0x01          
-#define lcd_Home               0x02          
-#define lcd_EntryMode          0x06         
-#define lcd_DisplayOff         0x08          
-#define lcd_DisplayOn          0x0F          
-#define Set5x7FontSize         0x20					
-#define eight_bit_mode		   0x38					     
-#define first_row              0x80         
-#define second_row             0xC0          
+//#define lcd_Clear              0x01          
+//#define lcd_Home               0x02          
+//#define lcd_EntryMode          0x06         
+//#define lcd_DisplayOff         0x08          
+//#define lcd_DisplayOn          0x0F          
+//#define Set5x7FontSize         0x20					
+//#define eight_bit_mode		   0x38					     
+//#define first_row              0x80         
+//#define second_row             0xC0          
 #define CURSOR_OFF             0x0C					 
 #define CURSOR_ON              0x0E		
 
