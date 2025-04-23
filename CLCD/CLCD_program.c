641d059cff02304d78230aa224682caac8f1103a
Randa Ayman <randaaysamir04@gmail.com>
2025-04-23 21:53:58 +0200

    CLCD_program.c

--- CLCD/CLCD_program.c
+++ CLCD/CLCD_program.c
@@ -15,24 +15,22 @@
 #include "CLCD_config.h"
 #include "STD_TYPES.h"
 #include "SYSTICK_interface.h"
-//INCLUDE SYSTICK INTERFACE & CHANGE ANY FUNCTION OF DELAY TO SISTICK DELAY 
-//ADJUST ALL GPIO FUNCTION AND IT'S INPUT 
-//add delay between each command 1 ms 
+
 STD_ERROR CLCD_8DataPinsInit (void)
 {  
     STD_ERROR ret = NOK;
-    GPIO_SetPortDir(DATA_PORT,PORT_OUTPUT);
+        GPIO_SetPortDir(DATA_PORT,PORT_OUTPUT);
 	GPIO_SetPinDir(CTRL_PORT,ENA,PIN_OUTPUT);
 	GPIO_SetPinDir(CTRL_PORT,RW,PIN_OUTPUT);
 	GPIO_SetPinDir(CTRL_PORT,RS,PIN_OUTPUT);
    
 
+         Systick_DelayIn_ms(1);
+        CLCD_8DataPinssendCommand(0x38);
         Systick_DelayIn_ms(1);
-        CLCD_voidSendCommand(0b00111000);
+        CLCD_8DataPinssendCommand(0xC);
         Systick_DelayIn_ms(1);
-        CLCD_voidSendCommand(0b00001100);
-        Systick_DelayIn_ms(1);
-        CLCD_voidSendCommand(0b00000001);
+        CLCD_8DataPinssendCommand(0x1);
         Systick_DelayIn_ms(1);
           // Set success
     ret = OK;
@@ -125,10 +123,11 @@ void CLCD_ClearDisplay(){
 
 void CLCD_voidSendSpecialCharacter(u8 Copy_u8BlockNum, u8 Copy_HorizontalValue,u8 copy_AbsVerticalValue,u8 *Pu8ArrayPattern){
     u8 Local_u8AddressPosition=Copy_u8BlockNum*8;
+	u8 i;
     //go to CGRAM -->CMD SETT_GCRAM_ADD
     CLCD_8DataPinssendCommand(Local_u8AddressPosition+lcd_CGRAM_START );
     //START DRAWING PATTERN
-    for (u8 i = 0; i < 8; i++)
+    for (i = 0; i < 8; i++)
     {
         CLCD_8DATASendData (Pu8ArrayPattern[i]);
     }
@@ -145,13 +144,14 @@ void CLCD_voidSendSpecialCharacter(u8 Copy_u8BlockNum, u8 Copy_HorizontalValue,u
 void CLCD_SendNum(u16 Num){
     u8 LOCAL_u8Num,LOCAL_u8counter=0;
     u8 arr[100]={0};
+	u8 i;
     do{
         LOCAL_u8Num=Num%10;
         Num/=10;
         arr[LOCAL_u8counter]=LOCAL_u8Num + '0';
         LOCAL_u8counter++;
     }while(Num!=0);
-    for (u8 i = LOCAL_u8counter; i >0 ; i--)
+    for (i = LOCAL_u8counter; i >0 ; i--)
     {
         CLCD_8DATASendData(arr[i-1]);
         Systick_DelayIn_ms(1);
