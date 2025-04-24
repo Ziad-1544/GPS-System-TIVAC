50176afbb2f9f6099d97323a12c08d96a70fcbe9
Randa Ayman <randaaysamir04@gmail.com>
2025-04-24 17:31:43 +0200

    LED_private.h

--- LED/LED_private.h
+++ LED/LED_private.h
@@ -11,10 +11,12 @@
 #ifndef LED_PRIVATE_H_
 #define LED_PRIVATE_H_
 
+// All internal LEDs are on Port C
 
-#define LED_PORT      PortC
 
-#define RED_LED_PIN     PIN0
-#define GREEN_LED_PIN   PIN1
+#define RED_LED_PIN     PIN1
+#define BLUE_LED_PIN    PIN2
+#define GREEN_LED_PIN   PIN3
+
 
 #endif
