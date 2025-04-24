649eb7bc8e841f3cfbfd1b6900cde364b03597f4
Randa Ayman <randaaysamir04@gmail.com>
2025-04-24 18:28:49 +0200

    SW_config.h

--- SW/SW_config.h
+++ SW/SW_config.h
@@ -14,20 +14,20 @@
 
 
 // Internal switches (onboard)
-#define SW1_PORT   PortD
+#define SW1_PORT   PortF
 #define SW1_PIN    PIN4
 #define SW1_RES    GPIO_PULL_UP
 
-#define SW2_PORT   PortD
+#define SW2_PORT   PortF
 #define SW2_PIN    PIN0
 #define SW2_RES    GPIO_PULL_UP
 
 // External switches (example on Port E)
-#define EXT_SW1_PORT   PortD
+#define EXT_SW1_PORT   PortF
 #define EXT_SW1_PIN    PIN2
 #define EXT_SW1_RES    GPIO_PULL_DOWN
 
-#define EXT_SW2_PORT   PortD
+#define EXT_SW2_PORT   PortF
 #define EXT_SW2_PIN    PIN3
 #define EXT_SW2_RES    GPIO_PULL_DOWN
 
