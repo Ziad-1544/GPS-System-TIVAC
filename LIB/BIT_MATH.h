8e088695eaa32431efe215506cd63b308bf0e812
jessy_samuel <jessicasamuel2004@yahoo.com>
2025-04-24 15:18:46 +0200

    removed no needed funcs

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -3,8 +3,8 @@
 ///////     Intro To Embedded Project     /////////
 //////           Layer:  LIB             /////////
 /////        Bit Manipulation (BIT)     /////////
-////         DATE:   16-3-2025         /////////
-///         version:   1.2            /////////
+////         DATE:   24-4-2025         /////////
+///         version:   1.3            /////////
 //         AUTHOR: Jessica Samuel    /////////
 /////////////////////////////////////////////  
 #ifndef BIT_MATH_H_
@@ -16,8 +16,5 @@
 #define GET_BIT(reg, bit)    ((reg & (1 << bit)) >> bit)
 #define IS_BIT_SET(reg, bit) ((reg & (1 << bit)) >> bit)
 #define IS_BIT_CLR(reg, bit) !((reg & (1 << bit)) >> bit)
-
-#define ROR(reg, num)        reg = (reg << (REGISTER_SIZE - num)) | (reg >> num)
-#define ROL(reg, num)        reg = (reg >> (REGISTER_SIZE - num)) | (reg << num)
 
 #endif
