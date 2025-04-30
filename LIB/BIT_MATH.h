069e688fd57d2ccad49248374edc1a16e820a714
jessy_samuel <jessicasamuel2004@yahoo.com>
2025-04-30 23:34:41 +0300

    updating BITMATH file

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -14,7 +14,7 @@
 #define CLR_BIT(reg, bit)    reg &= ~(1 << bit)
 #define TOG_BIT(reg, bit)    reg ^= (1 << bit)
 #define GET_BIT(reg, bit)    ((reg & (1 << bit)) >> bit)
-#define IS_BIT_SET(reg, bit) ((reg & (1 << bit)) >> bit)
-#define IS_BIT_CLR(reg, bit) !((reg & (1 << bit)) >> bit)
+#define IS_BIT_SET(reg, bit) ((reg & (1 << bit)) != 0)  
+#define IS_BIT_CLR(reg, bit) ((reg & (1 << bit)) == 0)
 
 #endif
