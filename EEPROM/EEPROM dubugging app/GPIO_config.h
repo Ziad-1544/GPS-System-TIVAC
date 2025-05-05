d7b5147bd6a611a1f62b8574ef667ced39071a2d
Mustafa EL-Sherif <elsherifmustafa04@gmail.com>
2025-05-05 03:16:02 +0300

    EEPROM verified

---
+++ EEPROM/EEPROM dubugging app/GPIO_config.h
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////
+////////      Ain Shams University        /////////
+///////     Intro To Embedded Project    /////////
+//////           Layer:  MCAL           /////////
+/////                GPIO              /////////
+////                Version:1.3       /////////
+///         DATE:   4-24-2025        /////////
+//         AUTHOR: Ziad Kassem      /////////
+////////////////////////////////////////////
+#ifndef GPIO_CONFIG_H
+#define GPIO_CONFIG_H
+
+#endif
