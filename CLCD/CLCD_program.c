361e6e4ff84737aa48f56662ceb6a6460ba244c7
Randa Ayman <randaaysamir04@gmail.com>
2025-05-10 16:50:16 +0300

    CLCD_program.c

--- CLCD/CLCD_program.c
+++ CLCD/CLCD_program.c
@@ -9,12 +9,12 @@
 ////////////////////////////////////////////
 
 
-#include "../../MCAL/GPIO/GPIO_interface.h" 
+#include "GPIO_interface.h" 
 #include "CLCD_interface.h"
 #include "CLCD_private.h"
 #include "CLCD_config.h"
-#include "../../LIB/STD/STD_TYPES.h"
-#include "../../MCAL/SYSTICK/SYSTICK_interface.h"
+#include "STD_TYPES.h"
+#include "SYSTICK_interface.h"
 
 
 STD_ERROR CLCD_StdErrorDataPinsInit (void)
